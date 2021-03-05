203368a7aad80254b4a17b9b32827ceef334caa6
ij_slango <junginje@gmail.com>
2021-03-05 20:56:04 +0900

    210305-22.등굣길-Gen

--- 알고리즘 스터디 2차/22.등굣길.py
+++ 알고리즘 스터디 2차/22.등굣길.py
@@ -1,22 +1,38 @@
 def solution(m, n, puddles):
     # 초기 맵 설정
     maps = [[0]*m for _ in range(n)]
-    maps[0] = [[1]*m]
-    for i in range(n):
-        maps[i][0] = 1
-    for y,x in puddles:
-        maps[y][x] = 0
+    for x, y in puddles:
+        maps[y-1][x-1] = -1
 
-    for i in range(1, m):
-        for j in range(1, n):
-            if maps[i][j]:
-                maps[i][j] += maps[i-1][j]
-                if j:
+    if n > 1:
+        for i in range(n):
+            if maps[i][0] == -1:
+                break
+            maps[i][0] = 1
+    if m > 1:
+        for i in range(m):
+            if maps[0][i] == -1:
+                break
+            maps[0][i] = 1
+
+    for i in range(1, n):
+        for j in range(1, m):
+            if maps[i][j] != -1:
+                if maps[i-1][j] != -1:
+                    maps[i][j] += maps[i-1][j]
+                if maps[i][j-1] != -1:
                     maps[i][j] += maps[i][j-1]
 
-    return maps[-1][-1]
+    return maps[-1][-1]%1000000007
+
 
 mm = 4
 nn = 3
 puddless = [[2,2]]
+
+
 print(solution(mm, nn, puddless))
+
+mm = 1
+nn = 5
+puddless = [[1,2]]
