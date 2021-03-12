17ac4bd84c0e400f18ac6990e6f508a468e226ff
ij_slango <junginje@gmail.com>
2021-03-12 21:55:17 +0900

    210312-24.섬연결, 코테 5.가방퀴즈-Upd

--- 알고리즘 스터디 2차 코테/05.가방 퀴즈.py
+++ 알고리즘 스터디 2차 코테/05.가방 퀴즈.py
@@ -1,5 +1,4 @@
 # 핵심 소스코드의 설명을 주석으로 작성하면 평가에 큰 도움이 됩니다.
-
 def chkact(action):
     actlst = action.split(' ')
     num = 0
@@ -44,6 +43,58 @@ def solution(n, actions):
             answer += 1
 
     return answer
+
+# # 문제 해설
+# def solution(n, actions):
+#     # 가방이 어디에 놓여있는지 나타내기 위한 리스트 선언
+#     # 각 인덱스는 가방의 번호를, 원소는 해당 가방이 놓여있는 가방의 번호를 나타냄 
+#     # ex) ground[1] = 2 : 1번 가방이 2번 가방 안에 놓여있음 
+#     # 0: 땅에 놓여있음을 의미 
+#     # 인덱스와 가방번호를 맞춰주기 위해 n+1 길이의 리스트로 생성 
+#     ground = [0] * (n+1)
+
+#     for action in actions:
+#         action = action.split(' ') # 공백을 기준으로 action 문자열 분리 
+#         if action[0] == 'PUT':
+#             i, j = int(action[1]), int(action[3])
+#             if (ground[i] | ground[j]) == 0: # 두 가방은 반드시 땅에 놓여있어야 함
+#                 ground[i] = j # i가 j안에 놓여있음을 마킹 
+#             else:
+#                 return -1
+#         elif action[0] == 'SET' :
+#             i = int(action[1])
+#             if ground[i] == 0: # 가방 i는 반드시 바닥에 놓여있어야 함 
+#                 # ground 리스트를 순회하며 가방이 i안에 있다면
+#                 # 바닥에 놓음(0으로 마킹)
+#                 for j in range(1, n+1):
+#                     if ground[j] == i: 
+#                         ground[j] = 0 
+#             else:
+#                 return -1
+#         elif action[0] == 'SWAP':
+#             i, j = int(action[1]), int(action[3])
+#             if (ground[i] | ground[j]) == 0: # 두 가방은 반드시 땅에 놓여있어야 함
+#                 # ground 리스트를 순회하며 
+#                 # i안에 있는 가방과 j안에 있는 가방을 바꿔줌 
+#                 for c in range(1, n+1):
+#                     if ground[c] == i:
+#                         ground[c] = j
+#                     elif ground[c] == j:
+#                         ground[c] = i
+#             else:
+#                 return -1
+
+
+#     # 완성된 ground 리스트에 대해
+#     # 바닥에 놓여진 가방의 개수를 카운트해준다
+#     # 만약, 가방이 자신의 가방 번호보다 더 작은 번호의 가방에 들어가있다면, 적절하지 않으므로 -1을 리턴한다 
+#     ans = 0
+#     for i in range(1, n+1):
+#         if ground[i] == 0:
+#             ans += 1
+#         elif ground[i] < i:
+#             return -1
+#     return ans
 
 
     # answer = 0
--- 알고리즘 스터디 2차/24.섬 연결하기.py
+++ 알고리즘 스터디 2차/24.섬 연결하기.py
@@ -1,22 +1,136 @@
 def solution(n, costs):
-    answer = 0
+    costs.sort(key=lambda x:(x[2], x[0], x[1]))
 
-    cost_map = [[0] * n for _ in range(n)]
-
-    for cost in costs:
-        cost_map[min(cost[:2])][max(cost[:2])] = cost[2]
-
-    for i in range(1, n):
-        nowlst = cost_map[i][:i]
-        tmplst = []
-        for j in nowlst:
-            if j:
-                tmplst.append(j)
-        answer += min(tmplst)
+    con = []
+    vstd = [False for _ in range(n)]
+    nowcost = costs.pop(0)
+    answer = nowcost[2]
+    con.extend(nowcost[:2])
+    vstd[nowcost[0]] = vstd[nowcost[1]] = True
+    while sum(vstd) < n:
+        for cost in costs:
+            if cost[0] in con and cost[1] in con:
+                continue
+            elif cost[0] in con or cost[1] in con:
+                if cost[0] not in con:
+                    answer += cost[2]
+                    vstd[cost[0]] = True
+                    con.append(cost[0])
+                elif cost[1] not in con:
+                    answer += cost[2]
+                    vstd[cost[1]] = True
+                    con.append(cost[1])
+                costs.remove(cost)
+                break
 
     return answer
 
 
+# n = 5
+# costs = [[0,1,1],[3,4,1],[1,2,2],[2,3,4]]
+# print(solution(n, costs))
+
+# n = 5
+# costs = [[0, 1, 1], [3, 1, 1], [0, 2, 2], [0, 3, 2], [0, 4, 100]]
+# print(solution(n, costs))
+
+# n = 3
+# costs = [[1,0,1], [0,2,1]]
+# print(solution(n, costs))
+
 n = 4
 costs = [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]
 print(solution(n, costs))
+
+# # 예시답안:
+# def solution(n, costs):
+#     answer = 0
+#     costs.sort(key=lambda x: x[2])
+#     visited = [True] + [False] * (n - 1)
+
+#     while not all(visited):
+#         for cur_node, next_node, cost in costs:
+#             print(visited)
+#             if visited[cur_node] and visited[next_node]:
+#                 continue
+#             if visited[cur_node] or visited[next_node]:
+#                 visited[cur_node] = visited[next_node] = True
+#                 answer += cost
+#                 break
+#     return answer
+
+
+# # 2nd try:
+# def solution(n, costs):
+#     costs.sort(key=lambda x:(x[2], x[0], x[1]))
+#     cost_map = [[0] * (i+1) for i in range(n)]
+#     for cost in costs:
+#         cost_map[max(cost[:2])][min(cost[:2])] = cost[2]
+
+#     for i in range(2, n):
+#         for j in range(n-i):
+#             cost_map[i+j][j] += min(cost_map[i+j-1][j], cost_map[i+j][j+1])
+
+#     return cost_map[-1][0]
+
+    # answer = 0
+    # costs.sort(key=lambda x:(x[2], x[0], x[1]))
+    # vstd = [False] * (n)
+    # con = []
+    # uncon = []
+
+    # for cost in costs:
+    #     if cost[0] in con or cost[1] in con:
+    #         if cost[0] not in con:
+    #             con.append(cost[0])
+    #         if cost[1] not in con:
+    #             con.append(cost[1])
+                
+    #         if not vstd[cost[0]] or not vstd[cost[1]]:
+    #             answer += cost[2]
+    #             if not vstd[cost[0]]:
+    #                 vstd[cost[0]] = True
+    #             if not vstd[cost[1]]:
+    #                 vstd[cost[1]] = True
+
+    # return answer
+
+
+# def solution(n, costs):
+#     cost_map = [[0] * (i+1) for i in range(n)]
+
+#     for cost in costs:
+#         cost_map[max(cost[:2])][min(cost[:2])] = cost[2]
+
+#     for i in range(2, n):
+#         for j in range(i):
+#             # if j == i-1:
+#             #     cost_map[i][j] += cost_map[i-1][j-1]
+#             # elif j:
+#             #     cost_map[i][j] += min(cost_map[i-1][j-1], cost_map[i-1][j])
+#             # else:
+#             #     cost_map[i][j] += cost_map[i-1][j]
+
+#             if cost_map[i][j]:
+#                 # if j:
+#                 #     cost_map[i][j] += min(cost_map[i-1][j-1], cost_map[i-1][j])
+#                 if j == i-1:
+#                     cost_map[i][j] += cost_map[i-1][j-1]
+#                 elif j:
+#                     cost_map[i][j] += min(cost_map[i-1][j-1], cost_map[i-1][j])
+#                 else:
+#                     cost_map[i][j] += cost_map[i-1][j]
+
+#         # nowlst = cost_map[i][:i]
+#         # tmplst = []
+#         # for j in nowlst:
+#         #     if j:
+#         #         tmplst.append(j)
+#         # answer += min(tmplst)
+    
+#     answer = []
+#     for i in cost_map[-1]:
+#         if i:
+#             answer.append(i)
+
+#     return min(answer)
