cb3437503e16dc8c7274f136c1e1990b18d93356
ij_slango <junginje@gmail.com>
2021-03-24 14:13:11 +0900

    210324-SCOFE21_문제풀이-Gen

--- SCOFE2021/03.상품_배치_추천.py
+++ SCOFE2021/03.상품_배치_추천.py
@@ -1,17 +1,24 @@
 import numpy as np
 
-N = input()
+N = int(input())
 tmp_maps = []
 for _ in range(N):
     tmp_maps.append(list(map(int, input())))
 maps = np.array(tmp_maps)
 
 ans = 0
-size = []
+size = [0] * (N+1)
 for siz in range(1, N+1):
-    now_range = N - i + 1
+    now_range = N - siz + 1
+    tmp_ans = 0
     for i in range(now_range):
         for j in range(now_range):
-            ans += maps[maps[i:i+siz, j:j+siz]].sum()
-
-print(ans)
+            if np.all(maps[i:i+siz, j:j+siz]):
+                tmp_ans += 1
+                ans += 1
+    size[siz] = tmp_ans
+    
+print(f'total: {ans}')
+for i in range(1, len(size)):
+    if size[i]:
+        print(f'size[{i}]: {size[i]}')
