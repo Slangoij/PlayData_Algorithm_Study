7e748ea28b6fc7eb5f373a4000b3d2de62099ec1
ij_slango <junginje@gmail.com>
2021-04-15 20:54:55 +0900

    210415-SCOFE_Programmer_Dir_moveto_Algorithm_Dir-U

--- Programmers/SkillTest_210326/2.스타수열.py
+++
@@ -1,9 +0,0 @@
-def solution(a):
-    num_lst = list(set(a))
-    cnt_lst = [a.count(num_lst[i]) for i in range(len(num_lst))]
-
-    cnt_lst.sort(reverse=True)
-    if len(cnt_lst)>1:
-        return min(sum(cnt_lst[1:]), cnt_lst[0]) * 2
-    else:
-        return 0
--- SCOFE2021/1차/02.배송_전략_실험.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-maps = list(map(int, input()))
-
-for i in range(2, N):
-    if maps[i]:
-        maps[i] = maps[i-1] + maps[i-2]
-
-print(maps[-1])
